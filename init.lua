f2744158bb24d97d0c4e06528bc85f91bd70fd22
Heejoon Lee <heejoon.lee@gmail.com>
2023-12-25 22:22:09 +0900

    obsidian과 copilot 플러그인 설치

--- init.lua
+++ init.lua
@@ -211,6 +211,45 @@ require('lazy').setup({
   --
   --    For additional information see: https://github.com/folke/lazy.nvim#-structuring-your-plugins
   { import = 'custom.plugins' },
+  
+  {
+    'epwalsh/obsidian.nvim',
+    version = "*",  -- recommended, use latest release instead of latest commit
+    lazy = true,
+    -- ft = 'markdown',
+    -- Replace the above line with this if you only want to load obsidian.nvim for markdown files in your vault:
+    event = {
+      -- If you want to use the home shortcut '~' here you need to call 'vim.fn.expand'.
+      -- E.g. "BufReadPre " .. vim.fn.expand "~" .. "/my-vault/**.md"
+      'BufReadPre C:/Users/wafe/Documents/obsidian/**.md',
+      'BufNewFile C:/Users/wafe/Documents/obsidian/**.md',
+    },
+    dependencies = {
+      -- Required.
+      'nvim-lua/plenary.nvim',
+    },
+    opts = {
+      workspaces = {
+        {
+          name = 'default',
+          path = 'C:/Users/wafe/Documents/obsidian',
+        },
+      },
+      completion = {
+        nvim_cmp = true,
+        min_chars = 2,
+      },
+      mappings = {},
+    },
+  },
+  
+  {
+    'github/copilot.vim',
+    filetypes = {
+      markdown = true,
+    },
+  },
+  
 }, {})
 
 -- [[ Setting options ]]
@@ -518,6 +557,23 @@ cmp.setup {
     { name = 'luasnip' },
   },
 }
+
+-- [[ obsidian ]]
+vim.keymap.set("n", "gf", function()
+  if require("obsidian").util.cursor_on_markdown_link() then
+    return "<cmd>ObsidianFollowLink<CR>"
+  else
+    return "gf"
+  end
+end, { noremap = false, expr = true })
+
+-- [[ copilot ]]
+vim.keymap.set('i', '<C-J>', 'copilot#Accept("<CR>")', {
+  expr = true,
+  replace_keycodes = false
+})
+vim.g.copilot_no_tab_map = true
+vim.g.copilot_filetypes = {markdown = true}
 
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
