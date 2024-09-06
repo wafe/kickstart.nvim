6a6b66b0fcd52ae65a3df0f3bf6085b9d69bf5ac
Heejoon Lee <heejoon.lee@gmail.com>
2024-09-06 15:08:07 +0900

    obsidian과 copilot 플러그인 설치

--- init.lua
+++ init.lua
@@ -935,7 +935,46 @@ require('lazy').setup({
   --
   --  Uncomment the following line and add your plugins to `lua/custom/plugins/*.lua` to get going.
   --    For additional information, see `:help lazy.nvim-lazy.nvim-structuring-your-plugins`
-  -- { import = 'custom.plugins' },
+  { import = 'custom.plugins' },
+
+  {
+    'epwalsh/obsidian.nvim',
+    version = "*",  -- recommended, use latest release instead of latest commit
+    lazy = true,
+    -- ft = 'markdown',
+    -- Replace the above line with this if you only want to load obsidian.nvim for markdown files in your vault:
+    event = {
+      -- If you want to use the home shortcut '~' here you need to call 'vim.fn.expand'.
+      -- E.g. "BufReadPre " .. vim.fn.expand "~" .. "/my-vault/**.md"
+      'BufReadPre C:/Users/wafe/Documents/obsidian/**.md',
+      'BufNewFile C:/Users/wafe/Documents/obsidian/**.md',
+    },
+    dependencies = {
+      -- Required.
+      'nvim-lua/plenary.nvim',
+    },
+    opts = {
+      workspaces = {
+        {
+          name = 'default',
+          path = 'C:/Users/wafe/Documents/obsidian',
+        },
+      },
+      completion = {
+        nvim_cmp = true,
+        min_chars = 2,
+      },
+      mappings = {},
+    },
+  },
+  
+  {
+    'github/copilot.vim',
+    filetypes = {
+      markdown = true,
+    },
+  },
+
 }, {
   ui = {
     -- If you are using a Nerd Font: set icons to an empty table which will use the
@@ -957,6 +996,23 @@ require('lazy').setup({
     },
   },
 })
+
+-- [[ obsidian ]]
+vim.keymap.set("n", "gf", function()
+  if require("obsidian").util.cursor_on_markdown_link() then
+    return "<cmd>ObsidianFollowLink<CR>"
+  else
+    return "gf"
+  end
+end, { noremap = false, expr = true })
+
+-- [[ copilot ]]
+vim.keymap.set('i', '<C-J>', 'copilot#Accept("<CR>")', {
+  expr = true,
+  replace_keycodes = false
+})
+vim.g.copilot_no_tab_map = true
+vim.g.copilot_filetypes = {markdown = true}
 
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
